27c049069fb6b360d9313feea573c3c57cb8a3b0
James Bradford <40271160+MarbleToast@users.noreply.github.com>
2021-01-11 16:18:24 +0000

    Create test_suite.py

---
+++ test_suite.py
@@ -0,0 +1,42 @@
+def test_list_is_palindromic(arr: list) -> bool:
+    assert arr == arr.reverse()
+
+def test_list_is_zig_zag(arr: list) -> bool:
+    for i in range(len(arr) - 1):
+        try:
+            assert(arr[i] % 2 != arr[i+1] % 2)
+        except IndexError:
+            pass
+
+# Fill out the function bodies so the tests pass
+
+"""
+create_palindromic_list() -> list
+
+Returns a list of length n that is a palindrome; i.e same
+elements in same order in both directions
+"""
+def create_palindromic_list(n) -> list:
+    pass
+
+
+"""
+create_zig_zag_list() -> list
+
+Returns a list of length n that alternates each element between
+odd and even
+"""
+def create_zig_zag_list(n) -> list:
+    pass
+
+
+if __name__ == "__main__":
+    try:
+        arr = create_palindromic_list()
+        test_list_is_palindromic(arr)
+
+        arr2 = create_zig_zag_list()
+        test_list_is_zig_zag(arr2)
+    except AssertionError:
+        print("Test failed")
+    
